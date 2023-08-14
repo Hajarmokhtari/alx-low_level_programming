b2f31dc86690e594dce280b26e3249fb5841fb60
Hajarmokhtari <moumenhajar@outlook.fr>
2023-08-14 05:38:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: print comb of numbers
  * Return: Always 0
- */
+*/
+
 int main(void)
 {
 	int n;
@@ -16,7 +18,7 @@ int main(void)
 			continue;
 		}
 		putchar(',');
-		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
