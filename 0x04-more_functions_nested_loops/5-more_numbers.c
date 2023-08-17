08ffb6374bcd6b6c19571cfaa563dc2900728663
Hajarmokhtari <moumenhajar@outlook.fr>
2023-08-17 04:57:29 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,17 +11,17 @@ void more_numbers(void)
 {
 	int num, row, count;
 
-	for (row = 1; row <= 10, row++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (count = 0, count <= 14, count++)
+		for (count = 0; count <= 14; count++)
 		{
 			num = count;
 			if (count > 9)
 			{
-				_putchar(1 + 48);
+				_putchar('1');
 				num = count % 10;
 			}
-			_putchar(num + 48);
+			_putchar(num + '0');
 		}
 		_putchar('\n');
 	}
