5bdfab8aef99b0638d5d4ba47b6d956630030dbc
Hajarmokhtari <moumenhajar@outlook.fr>
2023-08-22 09:11:21 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,11 +5,11 @@
  * %dest: char type string
  * %src: char type string
  * Description: Copy the string pointed to by pointer 'src' to
- * the buffer pointed to by 'dest'
+ *              the buffer pointed to by 'dest'
  * Return: Pointer to 'dest'
- */
+*/
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src);
 {
 	int i = -1;
 
