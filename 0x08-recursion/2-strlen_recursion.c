dc5d1f2f5c80ffc8fbcf57f3836b73694e3914c8
Hajarmokhtari <moumenhajar@outlook.fr>
2023-08-30 13:49:21 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_ recursion(char *s)
 {
-int ntr = 0;
+	int ntr = 0;
 
-if (*s > '\0')
-{
-	ntr += _strlen_recursion(s + 1) + 1;
-}
+	if (*s > '\0')
+	{
+		ntr += _strlen_recursion(s + 1) + 1;
+	}
 
-return (ntr);
+	return (ntr);
 }
